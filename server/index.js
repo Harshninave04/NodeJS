9ff5d307d6d6049819784ac71433fe7adbad9311
Harsh Ninave <harshninave58@gmail.com>
2023-10-07 02:41:01 +0530

    HTTP methods

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ const url = require("url");
 const myServer = http.createServer((req, res) => {
   // req -> request, res-> response
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}:${req.url} New request Received \n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New request Received \n`;
 
   const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
+  // console.log(myUrl);
 
   fs.appendFile("log.txt", log, (err, data) => {
     switch (myUrl.pathname) {
@@ -44,6 +44,12 @@ const myServer = http.createServer((req, res) => {
         const yourSearch = myUrl.query.search_query;
         res.end(`You have search for ${yourSearch}`);
         break;
+      case "/signup":
+        if (req.method === "GET") res.end("This is a signup form !");
+        else {
+          // DB query
+          res.method("successs");
+        }
       default:
         res.end("404 NOT FOUND");
     }
