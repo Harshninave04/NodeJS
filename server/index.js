aaa4865ec909f2e70ac84df66ec88174f227452c
Harsh Ninave <harshninave58@gmail.com>
2023-10-06 23:43:07 +0530

    URL Handling

--- server/index.js
+++ server/index.js
@@ -18,19 +18,31 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 const myServer = http.createServer((req, res) => {
   // req -> request, res-> response
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}:${req.url} New request Received \n`;
+
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Homepage !");
         break;
       case "/about":
-        res.end("hello, I am Harsh Ninave !");
+        const username = myUrl.query.myname;
+        res.end(`hello, ${username}`);
         break;
       case "/contact-us":
         res.end("Find me on twitter and github as Harshninave04");
+        break;
+      case "/search":
+        const yourSearch = myUrl.query.search_query;
+        res.end(`You have search for ${yourSearch}`);
         break;
       default:
         res.end("404 NOT FOUND");
